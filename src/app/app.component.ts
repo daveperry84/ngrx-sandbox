71b8f17fe68b1f3045f6a6cda757a24da5ba2311
Dave P <daveperry.dev@gmail.com>
2024-11-20 18:27:43 +0000

    Add form to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ngrx-sandbox';
+  form = this.formBuilder.group({
+    name: [''],
+    emailAddress: [''],
+    phoneNumber: [''],
+  })
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  saveDetails(): void {
+    console.log('Save Details');
+    // Do Something
+  }
 }
