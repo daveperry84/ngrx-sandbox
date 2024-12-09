26a6dd6a203e9c6952cb371dd95b57de1e7fa690
Dave P <daveperry.dev@gmail.com>
2024-12-09 12:30:47 +0000

    Add Q&A route and page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { NgrxFormComponent } from './features/ngrx-form/ngrx-form.component';
 import { HomeComponent } from './features/home/home.component';
 import { NgServicesFormComponent } from './features/ng-services-form/ng-services-form.component';
+import { QAndAComponent } from './features/q-and-a/q-and-a.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'ngrx-form', component: NgrxFormComponent },
     { path: 'ng-services-form', component: NgServicesFormComponent },
+    { path: 'q-and-a', component: QAndAComponent },
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
 ];
---
+++ src/app/features/q-and-a/q-and-a.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-q-and-a',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './q-and-a.component.html',
+  styleUrl: './q-and-a.component.scss'
+})
+export class QAndAComponent {
+
+}
