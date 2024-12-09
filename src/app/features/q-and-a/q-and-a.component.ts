8511e586ce713b7d1e331025c8966a0fe4c336f7
Dave P <daveperry.dev@gmail.com>
2024-12-09 13:04:51 +0000

    Add content to Q&A page

--- src/app/features/q-and-a/q-and-a.component.ts
+++ src/app/features/q-and-a/q-and-a.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion'
 
 @Component({
   selector: 'app-q-and-a',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [
+    MatIconModule,
+    MatExpansionModule,
+  ],
   templateUrl: './q-and-a.component.html',
   styleUrl: './q-and-a.component.scss'
 })
